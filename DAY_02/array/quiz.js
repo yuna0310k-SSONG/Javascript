96368609ad86bc1a734fc8c47c000c3ec1116c4f
Yuna <una3917@naver.com>
2025-09-16 17:42:21 +0900

    20250916-1

--- DAY_02/array/quiz.js
+++ DAY_02/array/quiz.js
@@ -1,6 +1,19 @@
+const fruit = ["apple", "banana", "kiwi"];
+
 const classroom = [
-  { name: "A", capacity: 20, student: 18, lecture: "java" },
-  { name: "B", capacity: 20, student: 19, lecture: "python" },
-  { name: "C", capacity: 20, student: 20, lecture: "javascript" },
+  { name: "A", capacity: 20, students: 18, lecture: "java" },
+  { name: "B", capacity: 20, students: 19, lecture: "python" },
+  { name: "C", capacity: 20, students: 20, lecture: "javascript" },
 ];
-const hope = prompt("듣고 싶은 과목은? ");
+
+const wish = prompt("듣고 싶은 과목명 입력(java,python,javascript)");
+if (classroom[0].lecture == wish) {
+  classroom[0].students = classroom[0].students + 1;
+} else if (classroom[1].lecture == wish) {
+  classroom[1].students = classroom[1].students + 1;
+} else if (classroom[2].lecture == wish) {
+  alert("불가!");
+} else {
+  console.log("없는과목입니당. 옆 그린아케데미 ㄱ");
+}
+console.log(classroom);
